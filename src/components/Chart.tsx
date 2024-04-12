e527159bacb8f3de6f065704092b5a5da736a214
se-ma2024 <94701414+se-ma2024@users.noreply.github.com>
2024-04-12 12:06:07 +0900

    年少人口、生産年齢人口、老年人口のグラフに切り替えるためのラジオボタンを追加

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -10,6 +10,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import axios from "axios";
+import "./Chart.css";
 
 interface PopulationData {
   prefCode: number;
@@ -23,6 +24,7 @@ interface Props {
 
 const PopulationGraph: React.FC<Props> = ({ selectedPrefectures }) => {
   const [populationData, setPopulationData] = useState<PopulationData[]>([]);
+  const [selectedDataType, setSelectedDataType] = useState<string>("総人口");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,10 +42,12 @@ const PopulationGraph: React.FC<Props> = ({ selectedPrefectures }) => {
             return {
               prefCode: parseInt(prefCode),
               prefName,
-              data: response.data.result.data[0].data.map((item: any) => ({
-                ...item,
-                prefecture: prefName,
-              })),
+              data: response.data.result.data
+                .find((item: any) => item.label === selectedDataType)
+                .data.map((item: any) => ({
+                  ...item,
+                  prefecture: prefName,
+                })),
             };
           }
         );
@@ -60,32 +64,84 @@ const PopulationGraph: React.FC<Props> = ({ selectedPrefectures }) => {
     } else {
       setPopulationData([]);
     }
-  }, [selectedPrefectures]);
+  }, [selectedPrefectures, selectedDataType]);
+
+  const handleDataTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedDataType(event.target.value);
+  };
 
   if (Object.keys(selectedPrefectures).length === 0) {
     return null;
   }
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
-        <CartesianGrid stroke="#f5f5f5" />
-        <XAxis dataKey="year" allowDuplicatedCategory={false} />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        {populationData.map((data, index) => (
-          <Line
-            key={index}
-            type="monotone"
-            dataKey="value"
-            name={`${data.prefName}`}
-            stroke={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
-            data={data.data}
+    <div className="chart-container">
+      <div className="radio-buttons-container">
+        <div className="radio-button">
+          <input
+            type="radio"
+            id="totalPopulation"
+            name="dataType"
+            value="総人口"
+            checked={selectedDataType === "総人口"}
+            onChange={handleDataTypeChange}
           />
-        ))}
-      </LineChart>
-    </ResponsiveContainer>
+          <label htmlFor="totalPopulation">総人口</label>
+        </div>
+        <div className="radio-button">
+          <input
+            type="radio"
+            id="youngPopulation"
+            name="dataType"
+            value="年少人口"
+            checked={selectedDataType === "年少人口"}
+            onChange={handleDataTypeChange}
+          />
+          <label htmlFor="youngPopulation">年少人口</label>
+        </div>
+        <div className="radio-button">
+          <input
+            type="radio"
+            id="workingAgePopulation"
+            name="dataType"
+            value="生産年齢人口"
+            checked={selectedDataType === "生産年齢人口"}
+            onChange={handleDataTypeChange}
+          />
+          <label htmlFor="workingAgePopulation">生産年齢人口</label>
+        </div>
+        <div className="radio-button">
+          <input
+            type="radio"
+            id="elderlyPopulation"
+            name="dataType"
+            value="老年人口"
+            checked={selectedDataType === "老年人口"}
+            onChange={handleDataTypeChange}
+          />
+          <label htmlFor="elderlyPopulation">老年人口</label>
+        </div>
+      </div>
+      <ResponsiveContainer>
+        <LineChart margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="year" allowDuplicatedCategory={false} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {populationData.map((data, index) => (
+            <Line
+              key={index}
+              type="monotone"
+              dataKey="value"
+              name={`${data.prefName}`}
+              stroke={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
+              data={data.data}
+            />
+          ))}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
